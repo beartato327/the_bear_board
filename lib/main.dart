410baf14612d14b4c546b3b4fead011875467cb2
Beartato327 <rcapurro@unomaha.edu>
2022-12-22 13:09:11 -0600

    Done with part 2, added quilted grid, profile btn, center appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:full_screen_image_null_safe/full_screen_image_null_safe.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -26,12 +30,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext build) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Bear Board")),
-      body: const Center(child: Text("Bear Board")),
+      appBar: AppBar(title: Text("The Bear Board", style: GoogleFonts.rubikMonoOne(textStyle: const TextStyle(letterSpacing: -2)),),centerTitle: true, actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.account_circle))],),
+      body: GridView.custom(
+        gridDelegate: SliverQuiltedGridDelegate(
+          crossAxisCount: 4,
+          mainAxisSpacing: 2,
+          crossAxisSpacing: 2,
+          repeatPattern: QuiltedGridRepeatPattern.inverted,
+          pattern: [
+            QuiltedGridTile(2, 2),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 1),
+            QuiltedGridTile(1, 2),
+          ],
+        ),
+        childrenDelegate: SliverChildBuilderDelegate(
+              (context, index) => GridTile(child: FullScreenWidget(child: Image.network("https://source.unsplash.com/random?sig=$index", fit: BoxFit.cover, )), ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {},
         label: const Text("Add"),
         icon: Icon(Icons.add_a_photo),
+        backgroundColor: Colors.red,
       ),
     );
   }
