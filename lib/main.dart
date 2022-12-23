93bb9d65898250463f3e22a6580723227a9fec2c
recap <recapurro327@gmail.com>
2022-12-22 23:05:38 -0600

    part 3 done, added firebase email auth and routes for profile use

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -6,12 +7,15 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseUIAuth.configureProviders([EmailAuthProvider()]);
   runApp(const MyApp());
 }
 
@@ -22,12 +26,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var providers = [EmailAuthProvider()];
     return MaterialApp(
+      initialRoute: '/',//FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/profile',
+      routes:  {
+        '/': (context) => const HomePage(),
+        '/sign-in': (context) {
+          return SignInScreen(
+            providers: providers,
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.pushReplacementNamed(context, '/');
+              }),
+            ],
+          );
+        },
+        '/profile': (context) {
+          return ProfileScreen(
+            providers: providers,
+            actions: [
+              SignedOutAction((context) {
+                Navigator.pushReplacementNamed(context, '/sign-in');
+              }),
+            ],
+          );
+        },
+      },
       title: 'The Bear Board',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      //home: const HomePage(),
     );
   }
 }
@@ -36,9 +65,9 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext build) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("The Bear Board", style: GoogleFonts.rubikMonoOne(textStyle: const TextStyle(letterSpacing: -2)),),centerTitle: true, actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.account_circle))],),
+      appBar: AppBar(title: Text("The Bear Board", style: GoogleFonts.rubikMonoOne(textStyle: const TextStyle(letterSpacing: -2)),),centerTitle: true, actions: [IconButton(onPressed: (){Navigator.pushNamed(context, FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/profile');}, icon: const Icon(Icons.account_circle))],),
       body: GridView.custom(
         gridDelegate: SliverQuiltedGridDelegate(
           crossAxisCount: 4,
@@ -56,7 +85,7 @@ class HomePage extends StatelessWidget {
               (context, index) => GridTile(child: FullScreenWidget(child: Image.network("https://source.unsplash.com/random?sig=$index", fit: BoxFit.cover, )), ),
         ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
+      floatingActionButton: FirebaseAuth.instance.currentUser == null ? Visibility(child: FloatingActionButton(onPressed: (){}, ), visible: false,) : FloatingActionButton.extended(
         onPressed: () {FirebaseFirestore.instance.collection('photos').snapshots().listen((data){print(data.docs[0]['photo']);});},
         label: const Text("Add"),
         icon: Icon(Icons.add_a_photo),
